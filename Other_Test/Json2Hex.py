0cc903b5a88b44da65061fdef3602dbf7e910bcc
M <m_snead@hotmail.com>
2025-04-10 21:35:59 -0500

    Update Json2Hex.py

--- Other_Test/Json2Hex.py
+++ Other_Test/Json2Hex.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+,#!/usr/bin/env python3
 import binascii
 import json
 import ecdsa
@@ -68,9 +68,9 @@ def push_data(data_hex):
 # Your provided (non-canonical) DER signature with sighash flag appended.
 # (This is the raw output from your signing routine; it might be non-canonical.)
 provided_sig_hex = (
-    "3045022100a1b2c3d4e5f67890abcdef01234567890abcdef01234567890"
-    "abcdef01234502201a2b3c4d5e6f7890abcdef01234567890abcdef012345"
-    "67890abcdef01234501"
+    "xxxxx"
+    "xxxxx"
+    "yuyy"
 )
 
 # Canonicalize the signature.
@@ -83,8 +83,8 @@ print("Canonical DER signature with sighash:", canonical_sig_hex)
 
 # Your public key in hex (uncompressed: 65 bytes or compressed: 33 bytes)
 public_key_hex = (
-    "041b2c906b7c481f4ab3d8189d911041157ff93543bd89c2267796c16b953d7e"
-    "cdd925d69974e471dee85a663811cd669417195a63d1550b77f3b37f465b3b6882"
+    "xxxx"
+    "xxxx"
 )
 
 # ======================================================
@@ -97,7 +97,7 @@ print("Rebuilt scriptSig:", scriptSig_hex)
 # Build the transaction input.
 #
 # Your previous transaction reference data.
-prev_txid_str = "39a7cf8a69891b86fabdc779b1e21d7c97522efd2954d7f26632dc0d4ff767df"
+prev_txid_str = "xxxx"
 vout = 0
 prev_txid = lx(prev_txid_str)
 outpoint = COutPoint(prev_txid, vout)
@@ -110,12 +110,12 @@ txin = CTxIn(outpoint, CScript(x(scriptSig_hex)), nSequence=4294967293)
 #
 # Output 0: 33,900,000,000 satoshis to a P2WPKH address.
 value0 = 33900000000
-scriptPubKey0_hex = "0014f5689400671c266948f407e484c0c54c663979ab"
+scriptPubKey0_hex = "xxxx"
 txout0 = CTxOut(value0, CScript(x(scriptPubKey0_hex)))
 
 # Output 1: 99,999,644 satoshis to another P2WPKH address.
 value1 = 99999644
-scriptPubKey1_hex = "001412ccc1e401ac8c4f2d93898d704d91626dbde916"
+scriptPubKey1_hex = "xxxx"
 txout1 = CTxOut(value1, CScript(x(scriptPubKey1_hex)))
 
 # ---------------------------------------------------------------------
