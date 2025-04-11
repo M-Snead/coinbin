e1f49c2653b4395b3f6ef7c096ce4d386af18951
M <m_snead@hotmail.com>
2025-04-10 21:32:29 -0500

    Update FixSig.py

--- Other_Test/FixSig.py
+++ Other_Test/FixSig.py
@@ -4,8 +4,8 @@ import ecdsa
 from ecdsa.util import sigencode_der, sigdecode_der
 
 # Replace these variables with your actual private key and transaction hex.
-private_key_hex = 'fe26dcb1b995d6168aedf3e64a21310c5f51d0572bff16725ad076deccc008a0'
-tx_hex = '0200000001df67f74f0ddc3266f2d75429fd2e52977c1de2b179c7bdfa861b89698acfa7390000000000fdffffff0200f398e407000000160014f5689400671c266948f407e484c0c54c663979ab9cdff5050000000016001412ccc1e401ac8c4f2d93898d704d91626dbde916387f0d00'
+private_key_hex = 'xxxx'
+tx_hex = 'xxxx'
 
 # Convert private key (must be 32-byte for SECP256k1) and transaction hex
 sk = ecdsa.SigningKey.from_string(bytes.fromhex(private_key_hex), curve=ecdsa.SECP256k1)
